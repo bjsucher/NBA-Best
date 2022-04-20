f65ebed8cc8e54f0e8225c6f7bd6fa28a9771590
jacksondial <61251187+jacksondial@users.noreply.github.com>
2022-04-20 14:45:09 -0400

    Finished team selection

--- model_construction.py
+++ model_construction.py
@@ -26,10 +26,24 @@ def get_max(column: str, position: str):
 # print(get_max("ThreePointersPct"))
 # print(get_max("Assists"))
 # print(get_max("Turnovers"))
-print("start")
-test_pos = "'PG', 'SG'"
-print(get_max("Assists / Turnovers", test_pos))
-print("break")
+# print("start")
+# test_pos = "'PG', 'SG'"
+# print(get_max("TwoPointersAttempted", test_pos))
+# print(get_max("TwoPointersPct", test_pos))
+
+# attempt2 = cur.execute("SELECT Name FROM PlayerStats WHERE TwoPointersAttempted = 16.2")
+# print(attempt2.fetchone())
+
+# the highest 2ptPct is .673, Matisse Thybulle, but his avg 2pAttempt is only
+# 2.4.... should I include him?
+# I think it is actually ok because we are also multiplying by the number of 2
+# pointers made, so his score wont actually be the highest (likely)
+
+# pct2 = cur.execute(
+#     "SELECT Name, TwoPointersAttempted FROM PlayerStats WHERE TwoPointersPct = .673"
+# )
+# print(pct2.fetchone())
+
 # My idea is to normalize each of the metrics by the maximum value in all of
 # the data, since this is a competition then we can compare individuals to
 # others
@@ -42,36 +56,48 @@ print("break")
 # UPDATE: I added in the funcitonality to only include players in that position
 # for the maximum calculation.
 
-ass_turn = cur.execute(
-    """SELECT Name FROM PlayerStats WHERE (Assists / Turnovers) > 7 AND
-    GamesPlayed > 15 AND ThreePointersAttempted > 2"""
-)
-print(ass_turn.fetchall())
+# ass_turn = cur.execute(
+#     """SELECT Name FROM PlayerStats WHERE (Assists / Turnovers) > 7 AND
+#     GamesPlayed > 15 AND ThreePointersAttempted > 2"""
+# )
+# print(ass_turn.fetchall())
 
-tyus_jones = cur.execute(
-    """SELECT 
-        GamesPlayed,
-        Assists,
-        Turnovers
-    FROM PlayerStats WHERE Name = 'Tyus Jones'
-    """
-)
-print(tyus_jones.fetchone())
+# tyus_jones = cur.execute(
+#     """SELECT
+#         GamesPlayed,
+#         Assists,
+#         Turnovers
+#     FROM PlayerStats WHERE Name = 'Tyus Jones'
+#     """
+# )
+# print(tyus_jones.fetchone())
+
+###############################################################################
+# Guards
+###############################################################################
 
 
-def normalize_query(position: str):
+def guard_normalize_query(position: str):
+    """Create columns that are normalized."""
     query = f"""
+        CREATE TABLE GuardsNormalizedStats AS
             SELECT
                 ps.Name,
                 Position,
-                ROUND(ThreePointersPct / {get_max("ThreePointersPct", position)},3),
-                ROUND((Assists / Turnovers) / {get_max("Assists / Turnovers", position)},3),
-                ROUND(Steals / {get_max("Steals", position)}, 3),
-                ROUND(OffensiveRebounds / {get_max("OffensiveRebounds", position)}, 3),
-                ROUND((FreeThrowsAttempted * FreeThrowPct) / 
-                    {get_max("FreeThrowsAttempted * FreeThrowPct", position)},3),
-                TwoPointersAttempted,
-                TwoPointersMade
+                ROUND(ThreePointersAttempted  * ThreePointersPct /
+                    {get_max("ThreePointersAttempted * ThreePointersPct",
+                    position)},3) AS j_threes,
+                ROUND((Assists / Turnovers) / {get_max("Assists / Turnovers",
+                    position)},3) AS j_atr,
+                ROUND(Steals / {get_max("Steals", position)}, 3) AS j_stl,
+                ROUND(OffensiveRebounds / {get_max("OffensiveRebounds",
+                    position)}, 3) AS j_or,
+                ROUND((FreeThrowsAttempted * FreeThrowPct) /
+                    {get_max("FreeThrowsAttempted * FreeThrowPct",
+                    position)},3) AS j_ft,
+                ROUND((TwoPointersAttempted * TwoPointersPct) /
+                    {get_max("TwoPointersAttempted * TwoPointersPct",
+                    position)},3) AS j_twos
             FROM PlayerStats ps
             LEFT JOIN Salary s
             ON ps.Name = s.Name
@@ -81,6 +107,160 @@ def normalize_query(position: str):
     return query
 
 
-guards = cur.execute(normalize_query("'PG', 'SG'"))
-for i in guards:
+cur.execute("DROP TABLE IF EXISTS GuardsNormalizedStats")
+# print(guard_normalize_query("'PG', 'SG'"))
+cur.execute(guard_normalize_query("'PG', 'SG'"))
+con.commit()
+
+# This was useful before I created the table as the select statement
+# guards = cur.execute(normalize_query("'PG', 'SG'"))
+# for i in guards:
+#     print(i)
+# guards = cur.execute("SELECT * FROM GuardsNormalizedStats")
+# for i in guards:
+#     print(i)
+
+
+def guard_selections(table: str, number_players: int):
+    "Make Selections."
+    query = f"""
+    SELECT 
+        Name,
+        (j_threes + j_atr + j_stl + j_or + j_ft + j_twos) * (100 / 6) AS rank
+    FROM {table}
+    ORDER BY rank DESC
+    LIMIT {number_players}
+    """
+    return query
+
+
+guard_ranked = cur.execute(guard_selections("GuardsNormalizedStats", 2))
+for i in guard_ranked:
     print(i)
+
+
+###############################################################################
+# Forwards
+###############################################################################
+
+
+def forward_normalize_query(position: str):
+    "Create columns that are normalized"
+    query = f"""
+        CREATE TABLE ForwardsNormalizedStats AS
+            SELECT
+                ps.Name,
+                Position,
+                ROUND(ThreePointersAttempted  * ThreePointersPct /
+                    {get_max("ThreePointersAttempted * ThreePointersPct",
+                    position)},3) AS j_threes,
+                ROUND((Assists / Turnovers) / {get_max("Assists / Turnovers",
+                    position)},3) AS j_atr,
+                ROUND(DefensiveRebounds / {get_max("DefensiveRebounds",
+                    position)}, 3) AS j_dr,
+                ROUND(OffensiveRebounds / {get_max("OffensiveRebounds",
+                    position)}, 3) AS j_or,
+                ROUND((FreeThrowsAttempted * FreeThrowPct) /
+                    {get_max("FreeThrowsAttempted * FreeThrowPct",
+                    position)},3) AS j_ft,
+                ROUND((TwoPointersAttempted * TwoPointersPct) /
+                    {get_max("TwoPointersAttempted * TwoPointersPct",
+                    position)},3) AS j_twos,
+                ROUND(Blocks / {get_max("Blocks", position)}, 3) AS j_blocks
+            FROM PlayerStats ps
+            LEFT JOIN Salary s
+            ON ps.Name = s.Name
+            WHERE ps.Position IN ({position})
+            ORDER BY s.Salary2122
+            """
+    return query
+
+
+cur.execute("DROP TABLE IF EXISTS ForwardsNormalizedStats")
+cur.execute(forward_normalize_query("'PF', 'SF'"))
+con.commit()
+
+
+def forward_selections(table: str, number_players: int):
+    "Make Selections."
+    query = f"""
+    SELECT 
+        Name,
+        (j_threes + j_atr + j_dr + j_or + j_ft + j_twos + j_blocks) * (100 / 7) AS rank
+    FROM {table}
+    ORDER BY rank DESC
+    LIMIT {number_players}
+    """
+    return query
+
+
+forward_ranked = cur.execute(forward_selections("ForwardsNormalizedStats", 2))
+for i in forward_ranked:
+    print(i)
+
+# testing = cur.execute(
+#     "SELECT COUNT(Name) FROM PlayerStats WHERE Position IN ('PF', 'SF')"
+# )
+# for i in testing:
+#     print(i)
+
+
+###############################################################################
+# Centers
+###############################################################################
+
+
+def center_normalize_query(position: str):
+    "Create columns that are normalized"
+    query = f"""
+        CREATE TABLE CentersNormalizedStats AS
+            SELECT
+                ps.Name,
+                Position,
+                ROUND((Assists / Turnovers) / {get_max("Assists / Turnovers",
+                    position)},3) AS j_atr,
+                ROUND(DefensiveRebounds / {get_max("DefensiveRebounds",
+                    position)}, 3) AS j_dr,
+                ROUND(OffensiveRebounds / {get_max("OffensiveRebounds",
+                    position)}, 3) AS j_or,
+                ROUND((FreeThrowsAttempted * FreeThrowPct) /
+                    {get_max("FreeThrowsAttempted * FreeThrowPct",
+                    position)},3) AS j_ft,
+                ROUND((TwoPointersAttempted * TwoPointersPct) /
+                    {get_max("TwoPointersAttempted * TwoPointersPct",
+                    position)},3) AS j_twos,
+                ROUND(Blocks / {get_max("Blocks", position)}, 3) AS j_blocks
+            FROM PlayerStats ps
+            LEFT JOIN Salary s
+            ON ps.Name = s.Name
+            WHERE ps.Position IN ({position})
+            ORDER BY s.Salary2122
+            """
+    return query
+
+
+cur.execute("DROP TABLE IF EXISTS CentersNormalizedStats")
+cur.execute(center_normalize_query("'C'"))
+con.commit()
+
+
+def center_selections(table: str, number_players: int):
+    "Make Selections."
+    query = f"""
+    SELECT 
+        Name,
+        (j_atr + j_dr + j_or + j_ft + j_twos + j_blocks) * (100 / 6) AS rank
+    FROM {table}
+    ORDER BY rank DESC
+    LIMIT {number_players}
+    """
+    return query
+
+
+center_ranked = cur.execute(center_selections("CentersNormalizedStats", 1))
+for i in center_ranked:
+    print(i)
+
+# testing = cur.execute("SELECT COUNT(Name) FROM PlayerStats WHERE Position IN ('C')")
+# for i in testing:
+#     print(i)
